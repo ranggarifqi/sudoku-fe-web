ebd7b7d6e1e281cc855e4b17ffb00d416b83a2f5
Rangga Rifqi <rangga@ranggarifqi.com>
2021-12-06 08:27:56 +0700

    Install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
-import { getSudokuBoard } from './common/api'
-import { decodeSolution } from './common/encryption'
-import { SudokuBoard } from './common/model'
+import { getSudokuBoard } from "./common/api";
+import { decodeSolution } from "./common/encryption";
+import { SudokuBoard } from "./common/model";
+
+import "./index.css";
 
 function App() {
-  const [board, setBoard] = useState<SudokuBoard>({ board: [], key: '' })
+  const [board, setBoard] = useState<SudokuBoard>({ board: [], key: "" });
 
   useEffect(() => {
     const getBoard = async () => {
-      const board = await getSudokuBoard()
-      setBoard(board)
-    }
-    getBoard()
-  }, [])
+      const board = await getSudokuBoard();
+      setBoard(board);
+    };
+    getBoard();
+  }, []);
 
-  const decodedSolution = decodeSolution(board.key)
-  console.log(decodedSolution)
+  const decodedSolution = decodeSolution(board.key);
+  console.log(decodedSolution);
 
   return (
-    <div>Hello World</div>
-  )
+    <div className="container mx-auto">
+      <div>Hello World</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
