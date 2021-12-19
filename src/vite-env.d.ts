48e6af87c62eb5bd80abeeb4b9217c700158faa0
Rangga Rifqi <rangga@ranggarifqi.com>
2021-12-19 21:45:26 +0700

    Fix ImprotMetaEnv

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv extends Readonly<Record<string, string>> {
+interface ImportMetaEnv {
   readonly VITE_API_URL: string
   readonly VITE_BASIC_AUTH_USERNAME: string
   readonly VITE_BASIC_AUTH_PASSWORD: string
