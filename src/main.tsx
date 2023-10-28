82013d870aa87bc3f3b15fc9003e5b45f9bfa597
Rangga Rifqi <rangga@ranggarifqi.com>
2023-10-28 14:02:06 +0700

    Upgraded React to v18

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./store";
 
-ReactDOM.render(
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
